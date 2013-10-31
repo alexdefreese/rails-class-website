dace7d72224e8356f09bd492b410f6786142ee1f
Alex DeFreese <alexdefreese@gmail.com>
2013-10-30 21:38:06 -0400

    Modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131031013138) do
+ActiveRecord::Schema.define(version: 20131031013657) do
+
+  create_table "blogposts", force: true do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
