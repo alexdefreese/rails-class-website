1fc168bd494289a71dde0325a96eafe366543066
William DeFreese <alexdefreese@gmail.com>
2013-11-10 00:50:33 -0500

    Remapped home to blogposts#index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ RailsClassWebsite::Application.routes.draw do
   get "pages/home"
   get "pages/schedule"
 
-  root 'pages#home'
+  root 'blogposts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
