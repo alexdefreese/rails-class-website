60e5337e80dcd2af23da7712fa2f08aaf039f97d
William DeFreese <alexdefreese@gmail.com>
2013-11-10 00:53:09 -0500

    Added Comments Model and Controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131031013657) do
+ActiveRecord::Schema.define(version: 20131110055219) do
 
   create_table "blogposts", force: true do |t|
     t.string   "title"
     t.integer  "user_id"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "blogpost_id"
+    t.text     "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
